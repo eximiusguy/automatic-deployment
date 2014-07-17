831aa2300cfdfb055227fe6d6305c17e2080920f
eximiusguy <eximiusguy@gmail.com>
2014-07-17 18:34:31 +0530

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,50 +1,39 @@
 <?php
 	/**
-	 * GIT DEPLOYMENT SCRIPT
+	 * A script to pull updates from git, so that it reflects on site
+	 * 
+	 * Git pull updates and update local folder.
 	 *
-	 * Used for automatically deploying websites via github or bitbucket, more deets here:
-	 *
-	 *		https://gist.github.com/1809044
 	 */
 
-	// The commands
+	// An array in which following commands will run in sequence.
 	$commands = array(
 		'echo $PWD',
 		'whoami',
 		'git pull',
 		'git status',
-		'git submodule sync',
-		'git submodule update',
-		'git submodule status',
 	);
 
-	// Run the commands for output
-	$output = '';
+	// Display it pretty formatted.
+	$htmlout = '';
 	foreach($commands AS $command){
-		// Run it
+		// Shell run it
 		$tmp = shell_exec($command);
 		// Output
-		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-		$output .= htmlentities(trim($tmp)) . "\n";
+		$htmlout .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+		$htmlout .= htmlentities(trim($tmp)) . "\n";
 	}
 
-	// Make it pretty for manual user access (and why not?)
 ?>
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8">
-	<title>GIT DEPLOYMENT SCRIPT</title>
+	<title>Deploy it</title>
 </head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<body style="font-weight: bold; padding: 0 10px;">
 <pre>
- .  ____  .    ____________________________
- |/      \|   |                            |
-[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
- |___==___|  /              &copy; oodavid 2012 |
-              |____________________________|
-
-<?php echo $output; ?>
+ <?php echo $htmlout; ?>
 </pre>
 </body>
 </html>
